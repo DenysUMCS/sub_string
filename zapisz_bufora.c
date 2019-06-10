12ad1b11c5bdcecd5c1f7c6e2b28c089f81e51b3
DenysUMCS <mars.kuty@gmail.com>
2019-06-10 20:50:05 +0200

    bufory

---
+++ zapisz_bufora.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#define MAX 10
+
+int main(){
+	int shmid, *buf;
+	shmid = shmget(45281, MAX*sizeof(int),IPC_CREAT | 0600);
+if(shmid == -1){
+	perror("Utworzenie segmentu pamienci wspoldzielonej\n");
+	exit(1);
+}
+buf=(int *)shmat(shmid,NULL,0);
+if(buf== NULL){
+	perror("Przylaczenie segmentu pamieci wspoldzielonej\n");
+	exit(1);
+}
+for(int i=0;i<10000;i++)
+	buf[i%MAX]=i;
+}
