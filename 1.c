aa5590c9b3edfb883b2f85c3c306688b188d33fd
DenysUMCS <mars.kuty@gmail.com>
2019-06-10 18:23:42 +0200

    fork

--- 1.c
+++ 1.c
@@ -1,45 +1,17 @@
-#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-#define MAX 512
-
-int main(int argc, char* argv[]){
-int src, dest;
-int buf[MAX];
-int lbajt;
-
-if(argc<3){
-	printf("Nie poprawna ilosc argumentow\n");
-	printf("Uzyj:\n");
-	printf("%s <plik zrodlowy> <plik docelowy>\n",argv[0]);
-}
-src=open(argv[1],O_RDONLY);
-if(src == -1){
-	printf("Blad otwarcia plika zrodlowego\n");
+int main(){
+printf("Pocatek\n");
+if(fork()==0){
+	execlp("ls","ls","-a",NULL);
+	perror("Blad uruchmienia\n");
 	exit(1);
 }
-dest=creat(argv[2],0640);
-if(dest == -1){
-	printf("Blad utworzenia plika docelowego\n");
-	exit(1);
-}
-
-while((lbajt=read(src,buf,MAX))>0){
-	if(write(dest,buf,lbajt)==-1){
-		printf("Blad zapisu pliku docelowego\n");
-		exit(1);
-	}
-}
-if(lbajt == -1){
-	printf("Blad odczytu pliku zrodlowego");
-	exit(1);
-}
-
-
-if(close(src)==-1 || close(dest)==-1){
-	printf("Blad zamkniecia plika\n");
-}
+wait(NULL);
+printf("Koniec\n");
 return 0;
 }
